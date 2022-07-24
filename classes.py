6aabc9ebead4421aa51aa6ed8668713d706d0257
Nadezhda <nadezhda.shatalina0@gmail.com>
2022-07-24 23:16:39 +0300

    added checking having stamina and fp

--- classes.py
+++ classes.py
@@ -37,27 +37,30 @@ class Base:
             self.stamina -= 10
             return f'Attack: -{self.damage}'
         else:
-            return f"Need a relax!"
+            return "Need a relax!"
 
     def go_to(self):
         if self.has_stamina():
             self.stamina -= 5
             return 5
         else:
-            return f"Need a relax!"
+            return "Need a relax!"
 
     def run(self):
         if self.has_stamina():
             self.stamina -= 10
             return 10
         else:
-            return f"Need a relax!"
+            return "Need a relax!"
 
     def is_alive(self):
         return self.hp > 0
 
     def death(self):
         del self
+
+    def ask_help(self):
+        return "Need help!"
 
     def __str__(self):
         return f"HP: {self.hp}\nFP: {self.fp}\nStamina: {self.stamina}"
@@ -71,34 +74,49 @@ class Wizard(Base):
     @comment
     def magical_light_attack(self, obj):
         obj.get_damaged(self.damage)
-        self.stamina -= 15
-        self.fp -= 10
-        return f"Magic attack: -{self.damage}"
+        if self.has_stamina() and self.fp >= 10:
+            self.stamina -= 15
+            self.fp -= 10
+            return f"Magic attack: -{self.damage}"
+        else:
+            return "Need a relax!"
 
     @comment
     def magical_heavy_attack(self, obj):
         damage = self.damage + self.damage * 0.3
         obj.get_damaged(damage)
-        self.stamina -= 20
-        self.fp -= 15
-        return f"Magic attack: -{damage}"
+        if self.has_stamina() and self.fp >= 15:
+            self.stamina -= 20
+            self.fp -= 15
+            return f"Magic attack: -{self.damage}"
+        else:
+            return "Need a relax!"
 
     @comment
     def healing(self, obj):
         setattr(obj, 'hp', getattr(obj, 'hp', 0) + 25)
-        self.stamina -= 20
-        self.fp -= 20
-        return f"Healing {getattr(obj, 'name', 'None')}"
+        if self.has_stamina() and self.fp >= 20:
+            self.stamina -= 20
+            self.fp -= 20
+            return f"Healing {getattr(obj, 'name', 'None')}"
+        else:
+            return "Need a relax!"
 
     @comment
     def recover_stamina(self, obj):
         setattr(obj, 'stamina', getattr(obj, 'stamina', 0) + 50)
-        self.stamina -= 15
-        self.fp -= 20
-        return f"Recovering {getattr(obj, 'name', 'None')}"
+        if self.has_stamina() and self.fp >= 20:
+            self.stamina -= 15
+            self.fp -= 20
+            return f"Recovering {getattr(obj, 'name', 'None')}"
+        else:
+            return "Need a relax!"
 
     def show_skills(self):
         return "Healing, Recover stamina"
+
+    def revive(self, obj):
+        setattr(obj, 'hp', 100)
 
 
 class Paladin(Base):
@@ -107,26 +125,25 @@ class Paladin(Base):
         self.damage = 13
 
     @comment
-    def healing(self, obj):
-        setattr(obj, 'hp', getattr(obj, 'hp', 0) + 30)
-        self.stamina -= 20
-        self.fp -= 20
-        return f"Healing {getattr(obj, 'name', 'None')}"
-
-    @comment
     def magical_light_attack(self, obj):
         obj.get_damaged(self.damage*0.7)
-        self.stamina -= 15
-        self.fp -= 10
-        return f"Magic attack: -{self.damage}"
+        if self.has_stamina() and self.fp >= 10:
+            self.stamina -= 15
+            self.fp -= 10
+            return f"Magic attack: -{self.damage}"
+        else:
+            return "Need a relax!"
 
     @comment
     def magical_heavy_attack(self, obj):
         damage = self.damage
         obj.get_damaged(damage)
-        self.stamina -= 20
-        self.fp -= 15
-        return f"Magic attack: -{damage}"
+        if self.has_stamina() and self.fp >= 15:
+            self.stamina -= 20
+            self.fp -= 15
+            return f"Magic attack: -{self.damage}"
+        else:
+            return "Need a relax!"
 
 
 if __name__ == "__main__":
